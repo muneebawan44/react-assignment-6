70e11c18a23c3bfdc60434db28395c198aa93308
muneebawan44 <143783326+muneebawan44@users.noreply.github.com>
2024-01-14 23:13:05 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './views/Dashboard/index.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
